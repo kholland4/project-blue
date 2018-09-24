92b76209ba25a93527d4a2e7d2fd7ebae4e01cf9
Alejandro Jimenez <ajim4268@github.com>
2018-09-23 22:59:59 -0600

    CSS link
    added CSS link for inputs

--- login.php
+++ login.php
@@ -6,6 +6,7 @@ require "util.php";
 <head>
 <title>Sign In - <?php echo $DISPLAY_NAME; ?></title>
 <link rel="stylesheet" type="text/css" href="loginStyle.css">
+<link rel="stylesheet" type="text/css" href="inButtStyle.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
