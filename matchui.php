49c2b92ea0777227879780952cee4c1197665044
Kyle <user@swift3.hollandnet.local>
2018-09-29 15:53:46 -0600

    Shared header CSS and interim styling for prefsui (still working on it)

--- matchui.php
+++ matchui.php
@@ -6,11 +6,15 @@ $userid = authenticate($conn);
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="matchui.css">
+<link rel="stylesheet" type="text/css" href="header.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <script src="matchui.js" type="text/javascript"></script>
 </head>
 <body>
-  <a id="back" href="<?php echo $BASE_URL; ?>"><i class="fas fa-2x fa-angle-left"></i></a>
+  <div id="appHeader">
+    <a id="backButton" href="javascript:history.go(-1);"><i class="fas fa-2x fa-angle-left"></i></a>
+    <span id="headerText"></span>
+  </div>
 </body>
 </html>
--- prefsui.php
+++ prefsui.php
@@ -25,13 +25,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <title>Your Interests - <?php echo $DISPLAY_NAME; ?></title>
 <script src="prefsui.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="prefsui.css">
+<link rel="stylesheet" type="text/css" href="header.css">
 <link rel="stylesheet" type="text/css" href="inButtStyle.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   <div id="appHeader">
-    <a id="backButton" href="<?php echo $BASE_URL; ?>"><i class="fas fa-2x fa-angle-left"></i></a>
+    <a id="backButton" href=""><i class="fas fa-2x fa-angle-left"></i></a>
     <span id="headerText"></span>
   </div>
   <div id="stage1">
