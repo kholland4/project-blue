823c46ffd5c7a90a521ee47977c6cbaf7d9b6612
Alejandro Jimenez <ajim4268@github.com>
2018-09-29 15:38:22 -0600

    Revert "Revert "back button""
    This reverts commit b399bdb947de8b93b928b69e5665b82f8d821412.

--- matchui.php
+++ matchui.php
@@ -6,8 +6,11 @@ $userid = authenticate($conn);
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="matchui.css">
 <script src="matchui.js" type="text/javascript"></script>
 </head>
 <body>
+  <a id="back" href="<?php echo $BASE_URL; ?>"><i class="fas fa-2x fa-angle-left"></i></a>
 </body>
 </html>
