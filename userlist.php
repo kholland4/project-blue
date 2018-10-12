902785e05737078e6b4cfb03296170af77c4547d
Alejandro Jimenez <ajim4268@github.com>
2018-10-11 18:19:40 -0600

    removed font awesome links

--- userlist.php
+++ userlist.php
@@ -9,7 +9,6 @@ $userid = authenticate($conn);
 <link rel="stylesheet" type="text/css" href="userlist.css">
 <link rel="stylesheet" type="text/css" href="header.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <script src="userlist.js" type="text/javascript"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
