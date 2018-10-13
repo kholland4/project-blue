9a29d796826c4d2669bd898066908b72977b06aa
Alejandro Jimenez <ajim4268@github.com>
2018-10-12 18:03:58 -0600

    corrected for chrome

--- index.php
+++ index.php
@@ -15,9 +15,6 @@ require "util.php";
   <div class ="slideContainer">
     <!-- images -->
     <div id="slide1">
-      <!-- <li class="liLogo">
-        <img class="logo" src="img/huugsClear.svg" style="max-width: 300px;" alt="<?php echo $DISPLAY_NAME; ?>">
-      </li> -->
       <div id="main">
       <?php
       $conn = create_sql_connection();
@@ -31,8 +28,7 @@ require "util.php";
         echo "<a href='" . $BASE_URL . "logout.php'>Log out</a><br>";
         echo "<a href='" . $BASE_URL . "userlist.php'>The List</a>";
       } else {
-        // echo "<li "
-        echo "<li class='liLogo'><img class='logo' src='img/huugsClear.svg' style='max-width: 300px;'></li>"; 
+        echo "<li class='liLogo'><img class='logo' src='img/huugsClear.svg' style='max-width: 300px;'></li>\n"; 
         echo "<a href='" . $BASE_URL . "login.php'><button>Log in</button></a><br>\n";
         echo "<a href='" . $BASE_URL . "register.php'><button>Register</button></a>";
       }
