d2fca83660d3d8f92f4d1e0f18202997536d64e4
Kyle <user@swift3.hollandnet.local>
2018-11-10 16:21:25 -0700

    committed what changes I have so you can commit w/o merge issues

--- getmatch.php
+++ getmatch.php
@@ -8,47 +8,51 @@ $userid = authenticate($conn);
 <?php
 header("Content-Type: text/json");
 
-$EXP = 1.5;
+if($MATCH_ALGO == "weighted") {
+  $EXP = 1.5;
 
-$target = prefs_get_arr($conn, $userid);
-$people = match_get_raw($conn, $userid);
-$scores = array();
-foreach($people as $person) {
-  $scoresRaw = array();
-  foreach($target as $t_p => $t_l) {
-    $found = false;
-    foreach($person["prefs"] as $p_p => $p_l) {
-      if($t_p == $p_p) { //found matching preference
-        $maxRange = ($PREF_LEVEL_MAX - $PREF_LEVEL_MIN + 1);
-        $p_l = min(max($p_l, $PREF_LEVEL_MIN), $PREF_LEVEL_MAX); //constrain
-        $t_l = min(max($t_l, $PREF_LEVEL_MIN), $PREF_LEVEL_MAX); //constrain
-        $s = $maxRange - abs($p_l - $t_l); //calculate the match difference (higher is better)
-        $s = pow($s, $EXP) / pow($maxRange, $EXP); //logarithmetic weighting and convert to percent
-        array_push($scoresRaw, array("matchLevel" => $s, "importance" => $t_l, "pref" => $t_p)); //TODO: logaritmetic importance
-        $found = true;
+  $target = prefs_get_arr($conn, $userid);
+  $people = match_get_raw($conn, $userid);
+  $scores = array();
+  foreach($people as $person) {
+    $scoresRaw = array();
+    foreach($target as $t_p => $t_l) {
+      $found = false;
+      foreach($person["prefs"] as $p_p => $p_l) {
+        if($t_p == $p_p) { //found matching preference
+          $maxRange = ($PREF_LEVEL_MAX - $PREF_LEVEL_MIN + 1);
+          $p_l = min(max($p_l, $PREF_LEVEL_MIN), $PREF_LEVEL_MAX); //constrain
+          $t_l = min(max($t_l, $PREF_LEVEL_MIN), $PREF_LEVEL_MAX); //constrain
+          $s = $maxRange - abs($p_l - $t_l); //calculate the match difference (higher is better)
+          $s = pow($s, $EXP) / pow($maxRange, $EXP); //logarithmetic weighting and convert to percent
+          array_push($scoresRaw, array("matchLevel" => $s, "importance" => $t_l, "pref" => $t_p)); //TODO: logaritmetic importance
+          $found = true;
+        }
+      }
+      if(!$found) {
+        array_push($scoresRaw, array("matchLevel" => 0.0, "importance" => $t_l, "pref" => $t_p));
       }
     }
-    if(!$found) {
-      array_push($scoresRaw, array("matchLevel" => 0.0, "importance" => $t_l, "pref" => $t_p));
+    
+    $score = 0;
+    $totalWeight = 0;
+    foreach($scoresRaw as $s) {
+      $score += $s["matchLevel"] * $s["importance"];
+      $totalWeight += $s["importance"];
     }
+    $score /= $totalWeight;
+    array_push($scores, array("person" => $person, "score" => $score, "detail" => $scoresRaw));
   }
-  
-  $score = 0;
-  $totalWeight = 0;
-  foreach($scoresRaw as $s) {
-    $score += $s["matchLevel"] * $s["importance"];
-    $totalWeight += $s["importance"];
-  }
-  $score /= $totalWeight;
-  array_push($scores, array("person" => $person, "score" => $score, "detail" => $scoresRaw));
-}
 
-//TODO: sort scores
-/*foreach($scores as $score) {
-  echo $score["person"]["username"] . " - " . $score["score"] . "\n";
-  foreach($score["detail"] as $d) {
-    echo "  " . str_pad(round($d["matchLevel"] * 100, 2) . "%", 6) . " " . str_pad($d["importance"], 2) . " " . $prefData[$d["pref"]]["name"] . "\n";
-  }
-}*/
-echo json_encode($scores);
+  //TODO: sort scores
+  /*foreach($scores as $score) {
+    echo $score["person"]["username"] . " - " . $score["score"] . "\n";
+    foreach($score["detail"] as $d) {
+      echo "  " . str_pad(round($d["matchLevel"] * 100, 2) . "%", 6) . " " . str_pad($d["importance"], 2) . " " . $prefData[$d["pref"]]["name"] . "\n";
+    }
+  }*/
+  echo json_encode($scores);
+} else if($MATCH_ALGO == "flat") {
+  
+}
 ?>
